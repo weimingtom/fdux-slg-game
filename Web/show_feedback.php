6099d99b733ed76380e33a335f831bf19ad7f50c
Argoran <argoran@gmail.com>
2011-11-25 19:03:47 +0000

    修改反馈显示页面排版

--- Web/show_feedback.php
+++ Web/show_feedback.php
@@ -6,17 +6,13 @@
 $con = mysql_connect('localhost', 'root', '') or die('Could not connect: ' . mysql_error());
 mysql_select_db('feedback') or die('Could not select database');
 
-$query = 'SELECT * FROM feedback';
+$query = 'SELECT * FROM `feedback` ORDER BY `feedback`.`id` DESC';
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 echo "<div class='db_table'>\n";
 while ($row = mysql_fetch_array($result)) {
     echo "\t<div class='db_row'>\n";
-    echo "\t\t<p>" . $row[0] . "</p>\n";
-	echo "\t\t<p>" . $row[1] . "</p>\n";
-	echo "\t\t<p>" . $row[2] . "</p>\n";
-	echo "\t\t<p>" . $row[3] . "</p>\n";
-	echo "\t\t<p>" . $row[4] . "</p>\n";
+    echo "\t\t<p>ID :  " . $row[0] . " | 时间 : " . $row[1] . " | 玩家 : " . $row[3] . " | 邮箱 : " . $row[4] . " | 反馈 : " . $row[2] . "</p>\n";
     echo "\t</div>\n";
     echo "\t<hr />\n";
 }
