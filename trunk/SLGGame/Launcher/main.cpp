a63d0ad14c87a65da5ac7c6f95da634fb3f73d65
chuangludeng@gmail.com <chuangludeng@gmail.com@af7bddde-8786-a52b-ccff-faa8b83c9d09>
2013-02-13 18:48:32 +0000

    +更新启动器

--- trunk/SLGGame/Launcher/main.cpp
+++ trunk/SLGGame/Launcher/main.cpp
@@ -1,8 +1,12 @@
 #include "launcher.h"
 #include <QtGui/QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
+	QTextCodec::setCodecForTr(QTextCodec::codecForLocale());
+	QTextCodec::setCodecForLocale(QTextCodec::codecForLocale());
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
 	QApplication a(argc, argv);
 	Launcher w;
 	w.show();
