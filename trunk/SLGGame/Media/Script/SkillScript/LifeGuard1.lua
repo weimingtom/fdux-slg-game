d7b5651657091a03915d0ee25f0f39bdcb02f980
w2y3q2@163.com <w2y3q2@163.com@af7bddde-8786-a52b-ccff-faa8b83c9d09>
2013-03-05 19:59:38 +0000

    最后修改

--- trunk/SLGGame/Media/Script/SkillScript/LifeGuard1.lua
+++ trunk/SLGGame/Media/Script/SkillScript/LifeGuard1.lua
@@ -1,11 +1,9 @@
 function onaffect()
 	local mid = ScriptCommonLib.GetString("modifierid");
 	local sid = ScriptCommonLib.GetTempString("squadid");
-	local eid = ScriptCommonLib.GetTempString("effectid");
 	if mid ~= "" then
 		SquadLib.RemoveModifier(sid, mid);
 	end
-	local lv = SquadLib.GetEffectLevel(sid, eid);
 	mid = SquadLib.ApplyModifier(sid, 2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 0.0 );
 	ScriptCommonLib.SetString("modifierid", mid);
 end
