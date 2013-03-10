3e99636a84a8eb6ff92128b5eabb772aaf9df429
chuangludeng@gmail.com <chuangludeng@gmail.com@af7bddde-8786-a52b-ccff-faa8b83c9d09>
2013-03-10 16:22:11 +0000

    *修正CrashReport无法工作

--- trunk/SLGGame/SLGGame/src/Ver.h
+++ trunk/SLGGame/SLGGame/src/Ver.h
@@ -1,3 +1,3 @@
-#define VER_Crash _T("Build:$WCREV$")
-#define VER_Subject _T("ForgottenBattlefield Build:$WCREV$ Error Report")
+#define VER_Crash _T("Build$WCREV$")
+#define VER_Subject _T("ForgottenBattlefield Build$WCREV$ Error Report")
 #define VER "Build:$WCREV$ "
