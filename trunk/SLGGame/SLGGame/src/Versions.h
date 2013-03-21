93c7c503f881e1faa13ed67110dd3a3d8f2fc986
chuangludeng@gmail.com <chuangludeng@gmail.com@af7bddde-8786-a52b-ccff-faa8b83c9d09>
2013-03-21 14:39:03 +0000

    *添加补给界面技能提示

--- trunk/SLGGame/SLGGame/src/Versions.h
+++ trunk/SLGGame/SLGGame/src/Versions.h
@@ -1,3 +1,3 @@
-#define VER_Crash _T("Build621")
-#define VER_Subject _T("ForgottenBattlefield Build621 Error Report")
-#define VER "Build:621 "
+#define VER_Crash _T("Build624")
+#define VER_Subject _T("ForgottenBattlefield Build624 Error Report")
+#define VER "Build:624 "
