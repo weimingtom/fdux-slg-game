95c0ba2a276d32469ba6df85b243956b9691b81a
chuangludeng@gmail.com <chuangludeng@gmail.com@af7bddde-8786-a52b-ccff-faa8b83c9d09>
2013-03-25 13:53:07 +0000

    *修正小地图

--- trunk/SLGGame/SLGGame/src/Versions.h
+++ trunk/SLGGame/SLGGame/src/Versions.h
@@ -1,3 +1,3 @@
-#define VER_Crash _T("Build630")
-#define VER_Subject _T("ForgottenBattlefield Build630 Error Report")
-#define VER "Build:630 "
+#define VER_Crash _T("Build631")
+#define VER_Subject _T("ForgottenBattlefield Build631 Error Report")
+#define VER "Build:631 "
