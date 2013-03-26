03e44f8db1b545f400fcc58d496bd077aaa5e61a
chuangludeng <chuangludeng@gmail.com>
2013-03-26 16:26:24 +0000

    *更新显卡选择

--- SLGGame/SLGGame/src/Versions.h
+++ SLGGame/SLGGame/src/Versions.h
@@ -1,3 +1,3 @@
-#define VER_Crash _T("Build631")
-#define VER_Subject _T("ForgottenBattlefield Build631 Error Report")
-#define VER "Build:631 "
+#define VER_Crash _T("Build633")
+#define VER_Subject _T("ForgottenBattlefield Build633 Error Report")
+#define VER "Build:633 "
