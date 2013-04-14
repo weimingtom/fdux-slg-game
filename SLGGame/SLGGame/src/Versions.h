f96b76e79198ec3cf649a7b9e969ae17cc7e152d
chuangludeng <chuangludeng@gmail.com>
2013-04-14 13:31:36 +0000

    *小地图....

--- SLGGame/SLGGame/src/Versions.h
+++ SLGGame/SLGGame/src/Versions.h
@@ -1,3 +1,3 @@
-#define VER_Crash _T("Build634")
-#define VER_Subject _T("ForgottenBattlefield Build634 Error Report")
-#define VER "Build:634 "
+#define VER_Crash _T("Build644")
+#define VER_Subject _T("ForgottenBattlefield Build644 Error Report")
+#define VER "Build:644 "
